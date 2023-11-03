8eaf534618b26fb7f7f8c98bbd47487b1a105124
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-02 21:44:41 -0400

    feat: add methods

--- csharp-exercise-stringbuilder/Entities/Post.cs
+++ csharp-exercise-stringbuilder/Entities/Post.cs
@@ -14,9 +14,38 @@ namespace csharp_exercise_stringbuilder.Entities
         public int Likes { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
 
-        public Post()
+        public Post(DateTime moment, string title, string content, int likes)
         {
-            
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void addComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void removeComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes -");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments");
+            foreach (var item in Comments)
+            {
+                sb.AppendLine(item.Text);
+            }
+            return sb.ToString();
         }
     }
 }
